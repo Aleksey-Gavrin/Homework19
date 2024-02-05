eaaeed39b16996f6e0f2d2b0131b0127050a7aee
Aleksey-Gavrin <darkgarvin@gmail.com>
2024-02-05 22:54:29 +0300

    minor refactoring

--- src/main/java/pro/sky/homework19/service/EmployeeService.java
+++ src/main/java/pro/sky/homework19/service/EmployeeService.java
@@ -16,6 +16,7 @@ import java.util.Map;
 @Service
 public class EmployeeService {
     private final Map<String, Employee> employeeMap;
+
     public EmployeeService() {
         this.employeeMap = new HashMap<>();
     }
@@ -24,51 +25,45 @@ public class EmployeeService {
         return employeeMap;
     }
 
-    final int MAX_EMPLOYEES = 15;
+    private final int MAX_EMPLOYEES = 15;
 
-    public boolean checkStringInput(String str) {
-        return StringUtils.isAlpha(str);
+    public static void checkStringInput(String str1, String str2) {
+        if (!(StringUtils.isAlpha(str1) && StringUtils.isAlpha(str2))) {
+            throw new BadRequestException();
+        }
     }
 
     public Employee addEmployee(String firstName, String lastName, double salary, int department) {
+        checkStringInput(firstName, lastName);
         if (employeeMap.size() >= MAX_EMPLOYEES) {
             throw new EmployeeStorageIsFullException();
         }
-        if (checkStringInput(firstName) && checkStringInput(lastName)) {
-            String firstNameCapitalized = StringUtils.capitalize(firstName);
-            String lastNameCapitalized = StringUtils.capitalize(lastName);
-            Employee empl = new Employee(firstNameCapitalized, lastNameCapitalized, salary, department);
-            if (employeeMap.containsValue(empl)) {
+        Employee empl = new Employee(firstName, lastName, salary, department);
+        if (employeeMap.containsValue(empl)) {
             throw new EmployeeAlreadyAddedException();
-            } else {
+        } else {
             employeeMap.put(empl.getHashKey(), empl);
             return empl;
-            }
-        } else {
-            throw new BadRequestException();
         }
+
     }
 
     public Employee findEmployee(String firstName, String lastName) {
-        if (checkStringInput(firstName) && checkStringInput(lastName)) {
-            String hashKey = (firstName + lastName).toLowerCase();
-            if (employeeMap.containsKey(hashKey)) {
-                return employeeMap.get(hashKey);
-            } else {
-                throw new EmployeeNotFoundException();
-            }
+        checkStringInput(firstName, lastName);
+        String hashKey = (firstName + lastName).toLowerCase();
+        if (employeeMap.containsKey(hashKey)) {
+            return employeeMap.get(hashKey);
         } else {
-            throw new BadRequestException();
+            throw new EmployeeNotFoundException();
         }
     }
+
     public Employee removeEmployee(String firstName, String lastName) {
-        if (checkStringInput(firstName) && checkStringInput(lastName)) {
-            Employee empl = findEmployee(firstName, lastName);
-            return employeeMap.remove(empl.getHashKey());
-        } else {
-            throw new BadRequestException();
-        }
+        checkStringInput(firstName, lastName);
+        Employee empl = findEmployee(firstName, lastName);
+        return employeeMap.remove(empl.getHashKey());
     }
+
     public Collection<Employee> printAll() {
         return Collections.unmodifiableCollection(employeeMap.values());
     }
